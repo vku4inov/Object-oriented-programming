93e78f35b85d08b65b39685307c1cfb94a5ec127
DaGGeR <79262707+vku4inov@users.noreply.github.com>
2021-03-03 12:21:10 +0300

    Add files via upload

--- test.java
+++ test.java
@@ -1,30 +1,24 @@
+package Lab1;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
 
 public class test {
     public static void main(String[] args) {
-        ArrayList<Integer> arrayNum = new ArrayList<>();
+        ArrayList<Integer> arrayNumbers = new ArrayList<>();
         Scanner scan = new Scanner(System.in);
         System.out.println("Введите ваш набор значений");
-        System.out.println("После ввода введите 'stop' и нажмите Enter, или введите любую букву ");
+        System.out.println("После окончания ввода введите любую букву");
         while (true){
        if(scan.hasNextInt()) {
-           arrayNum.add(scan.nextInt());
-       }
-       else { break;
-         /** if (scan.hasNextLine()){
-              scan.close();
-              System.out.println("Ваш набор:");
-              System.out.println(arrayNum);
-              break;}
-          else break; **/
-       }
-}
-        Collections.sort(arrayNum);
-        System.out.println("Отсортированный набор:");
-        System.out.println(arrayNum);
-        System.out.println("Минимальный элемент " + arrayNum.get(0));
-        System.out.println("Максимальынй элемент " + arrayNum.get(arrayNum.size()-1));
-    }
-}
+           arrayNumbers.add(scan.nextInt());
+                             }
+       else
+           break;
+                    }
+        System.out.println(arrayNumbers);
+        System.out.println(Collections.min(arrayNumbers));
+        System.out.println(Collections.max(arrayNumbers));
+                                            }
+                }
